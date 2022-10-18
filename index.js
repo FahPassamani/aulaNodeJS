b761b851c554bce42e7d51a5f43a40093eb34881
FahPassamani <86082505+FahPassamani@users.noreply.github.com>
2022-10-18 00:46:37 -0300

    Update msg index

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use('/produtos', productRoutes);
 
-app.get('/', (req, res) => res.send('Olá da Homepage'));
+app.get('/', (req, res) => res.send('Homepage'));
 
 app.listen(PORT, () => console.log(`Server rodando na porta: ${PORT}`));
